a8f8d35c43bbb96281651b58cea4002451b3556f
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2013-03-12 23:42:13 +0100

    Tentative de connexion mongo dans un test

---
+++ test/db_access_int_test.dart
@@ -0,0 +1,25 @@
+library db_acccess_test;
+
+import 'package:unittest/unittest.dart';
+import 'package:higgins_server/higgins_server.dart';
+
+BuildDao buildDao ;
+
+main(){
+  group('Configuration file', () {
+    setUp(() => _setUp());
+    test('should find all', () => should_find_all());
+  });
+}
+
+_setUp(){
+  initMongo("mongodb://localhost");
+  buildDao = new BuildDao();
+  // TODO insérer des données
+}
+
+
+should_find_all(){
+  // TODO test async
+  //buildDao.all()
+}
